cc2c75374d5e5b90fee757653a1b8be223c1a048
Babar Saleh Hayat <166165086+babarsaleh1@users.noreply.github.com>
2025-01-30 23:35:08 +0500

    Update beta-build.sh
    updated

--- .buildkite/commands/beta-build.sh
+++ .buildkite/commands/beta-build.sh
@@ -14,3 +14,4 @@ for aab in build/*.aab; do
   buildkite-agent artifact upload "$aab"
   echo "<a href="artifact://$aab">$(basename "$aab")</a>" | buildkite-agent annotate --style info --context "beta-build-$aab"
 done
+bundle exec fastlane build_beta app:$1 skip_confirm:true skip_prechecks:true upload_to_play_store:true.fix
